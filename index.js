2f6907f8b412ec654a1e8db384740040d3e15ae4
Agop Shirinian <agop@loewy.com>
2015-06-19 18:41:51 -0400

    Added ability to pass project's gulp instance, which is useful when testing with npm link Fixed logic error calling task builders

--- index.js
+++ index.js
@@ -34,14 +34,29 @@ function relPath(base, filePath)
 }
 
 module.exports = (function() {
-	function assets(configCb)
+	function assets(configCb, g)
 	{
+		if (typeof g !== 'undefined')
+		{
+			/**
+			 * When using npm link for testing, node modules become duplicated (including gulp). Because of this, gulp fails
+			 * to run correctly - it sees the gulp installation in the project, but it also sees a separate gulp installation
+			 * in this package. Therefore, this package ends up adding tasks to its own gulp instance, and gulp does not
+			 * see these tasks, because it's looking at the gulp instance from the project.
+			 *
+			 * To combat this, we allow the project to pass in its own gulp instance to overwrite ours. If npm link was used,
+			 * this resolves the issue with the duplicate gulp package. If npm link was not used, then the project's gulp instance
+			 * will match our gulp instance regardless, so this assignment will be harmless.
+			 */
+			gulp = g;
+		}
+
 		if (typeof configCb === 'function')
 		{
 			configCb.call(assets);
 		}
 
-		var tasks = this.tasks;
+		var tasks = assets.tasks;
 
 		for (var field in tasks)
 		{
@@ -52,7 +67,7 @@ module.exports = (function() {
 
 			var task = tasks[field];
 
-			task.call(this);
+			task.call(assets);
 		}
 	}
 
